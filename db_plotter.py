711b3d8c9d501605da9796f7dbc72fd4a81d9dad
Nathan Mattick <nathan.mattick@petards.com>
2022-01-24 08:42:33 +0000

    Remove the lower(), it hides errors

--- db_plotter.py
+++ db_plotter.py
@@ -2,14 +2,16 @@
 
 # Script to plot the waypoints, GFs and routes
 
-import inspect
 import os
 import sqlite3
 import sys
 
 import folium
 
-colour_wp = 'red'
+colour_wp =         'blue'
+colour_route_bi =   'black'
+colour_route_down = 'red'
+colour_route_up =   'green'
 
 # def log_error():
 #     print("Error: " + inspect.currentframe().f_code.co_name + "()")
@@ -25,7 +27,7 @@ class WayPoint:
             print("Error: can't add duplicate wp: " + wp.name)
 
     def __init__(self, name, lat, lng, r):
-        self.name = name.lower()
+        self.name = name
         self.lat = lat
         self.lng = lng
         self.r = r
@@ -44,7 +46,7 @@ class WayPoint:
 
     @classmethod
     def get_range(cls):
-        gps = list(WayPoint.waypoints.values())
+        gps = list(cls.waypoints.values())
         lat_range = (min(gps, key=lambda item: item[0])[0],
                      max(gps, key=lambda item: item[0])[0])
         lng_range = (min(gps, key=lambda item: item[1])[1],
@@ -53,21 +55,21 @@ class WayPoint:
 
     @classmethod
     def plot_waypoints(cls, map):
-        for wp in WayPoint.waypoints:
+        for wp in cls.waypoints:
             # Add wp marker to the map
-            lat, lng, r = WayPoint.waypoints[wp]
+            lat, lng, r = cls.waypoints[wp]
             folium.Marker([lat, lng], popup=wp + ", gf=" + str(r) + "m").add_to(map)
             folium.Circle((lat, lng), radius=r, color=colour_wp).add_to(map)
 
     @classmethod
     def exists(cls, name):
-        return (name in WayPoint.waypoints)
+        return (name in cls.waypoints)
 
 
-class Map:
+class Map (folium.folium.Map):
     def __init__(self, lat, lng):
         # Centre the map on the central wp location
-        self.map = folium.Map(prefer_canvas=True, zoom_start=10, location=(lat, lng))
+        folium.Map(prefer_canvas=True, zoom_start=10, location=(lat, lng))
 
 
 class Route:
@@ -92,7 +94,7 @@ class Route:
             for wp in c.execute("select waypoint_name, waypoint_lat, waypoint_long, waypoint_radius from waypoint "
                                 "order by waypoint_name asc"):
                 name, lat, lng, r = wp
-                WayPoint(name.lower(), lat, lng, r)
+                WayPoint(name, lat, lng, r)
 
         except Exception as e:
             print(e)
@@ -114,16 +116,15 @@ class Route:
         if not WayPoint.exists(to):
             print("Error: " + wp + ": " + direction.upper() + " '" + to + "' unknown")
             return False
+        return True
 
     def plot_routes(self, map):
         c = self.db.cursor()
 
-        # Read (lowercase wp) routes into up/down arrays
         for route in c.execute("select waypoint_name, up_station_name, up_distance, down_station_name, "
                                "down_distance from route order by waypoint_name asc"):
             try:
                 wp_name, up, up_dist, down, down_dist = route
-                [wp_name, up, down] = [x.lower() for x in (wp_name, up, down)]
             except Exception as e:
                 print("sql route")
                 print(e)
@@ -142,18 +143,17 @@ class Route:
                 continue
 
             if (up, wp) in Route.routes_down:
-                Route.routes_down.remove((up, wp))
-                folium.PolyLine([(self.waypoints[wp][:2], self.waypoints[up][:2])],
-                                color="black",
+                folium.PolyLine([(WayPoint.waypoints[wp][:2], WayPoint.waypoints[up][:2])],
+                                color=colour_route_bi,
                                 weight=5,
                                 fill_opacity=0.5,
                                 opacity=1,
                                 line_opacity=0.5,
-                                tooltip=wp + " <-> " + up
+                                tooltip=wp + " BI " + up
                                 ).add_to(map)
             else:
-                folium.PolyLine([(self.waypoints[wp][:2], self.waypoints[up][:2])],
-                                color="green",
+                folium.PolyLine([(WayPoint.waypoints[wp][:2], WayPoint.waypoints[up][:2])],
+                                color=colour_route_up,
                                 weight=5,
                                 fill_opacity=0.5,
                                 opacity=1,
@@ -166,8 +166,11 @@ class Route:
             if not Route.is_valid(wp, down, 'DOWN'):
                 continue
 
-            folium.PolyLine([(self.waypoints[wp][:2], self.waypoints[down][:2])],
-                            color="yellow",
+            if (down, wp) in Route.routes_up:
+                continue
+
+            folium.PolyLine([(WayPoint.waypoints[wp][:2], WayPoint.waypoints[down][:2])],
+                            color=colour_route_down,
                             weight=5,
                             fill_opacity=0.5,
                             opacity=1,
@@ -189,9 +192,9 @@ def main(argv):
     route = Route('asdo_config.db')
 
     lat, lng = WayPoint.get_centre()
-    map = Map(lat, lng)
-    route.plot_routes(map)
+    map = folium.Map(prefer_canvas=True, zoom_start=10, location=(lat, lng))
     WayPoint.plot_waypoints(map)
+    route.plot_routes(map)
 
     map.save('map.html')
     print("\nDone: open map.html in browser")
